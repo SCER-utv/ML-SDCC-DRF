a8ef751a9d3a504f91ea1cca7cc825567ff4ad7d
casolaroSalvatore <150945794+casolaroSalvatore@users.noreply.github.com>
2026-04-27 21:06:41 +0200

    Update baseline.py

--- baseline.py
+++ baseline.py
@@ -91,7 +91,7 @@ def save_unified_baseline_metrics(dataset, n_trees, train_time, inf_time, metric
     Saves all metrics in a single global CSV file.
     Metrics not relevant to the current task will remain empty to keep the format clean.
     """
-    s3_key = "results/unified_baseline_results.csv"
+    s3_key = "metrics/unified_baseline_results.csv"
 
     # Prepare an "empty" row with all possible columns
     row_data = {
